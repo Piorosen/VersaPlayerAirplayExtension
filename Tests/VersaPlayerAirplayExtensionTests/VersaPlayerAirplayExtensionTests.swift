5a04e275a167fa2581436df05ec3906a12f40f83
Piorosen <aoikazto@gmail.com>
2021-12-19 17:33:22 +0900

    support spm 5.4

---
+++ Tests/VersaPlayerAirplayExtensionTests/VersaPlayerAirplayExtensionTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VersaPlayerAirplayExtension
+
+final class VersaPlayerAirplayExtensionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VersaPlayerAirplayExtension().text, "Hello, World!")
+    }
+}
